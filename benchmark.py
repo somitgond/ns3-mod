1ed5ae613e9853f60fa5785af6d4ea1a2af1fbf6
Somit Gond <somitgond2@gmail.com>
2025-03-24 13:02:21 +0530

    adding python3 program to benchmark custom aqm

---
+++ benchmark.py
@@ -0,0 +1,11 @@
+# Author: Somit Gond
+# Date: 24/03/2025
+
+'''
+Program to benchmark threshold based queuing policy against droptail
+'''
+
+import os
+
+if __name__ == "__main__":
+    print("Hello, world!")
